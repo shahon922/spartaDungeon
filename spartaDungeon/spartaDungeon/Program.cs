0c09b58f48ca4fc6729495e4433ae1c8ec41fb5f
shahon922 <shahon922@naver.com>
2024-01-03 17:56:48 +0900

    아이템 리스트 추가

--- spartaDungeon/spartaDungeon/Program.cs
+++ spartaDungeon/spartaDungeon/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Text;
+
 namespace spartaDungeon
 {
     internal class Program
@@ -7,19 +9,66 @@ namespace spartaDungeon
         static string name;
         static int level = 1;
         static int attack = 10;
-        static int defense = 5;
+        static int defense = 5; 
         static int hp = 100;
         static int gold = 1500;
+        //public List<Item> itemList;
+
+        class Item
+        {
+            public string name;
+            public int count;
+            public int value;
+            public string options;
+            public int price;
+
+            public Item(string name, int value, string options, int price)
+            {
+                this.name = name;
+                this.count = 1;
+                this.value = value;
+                this.options = options;
+                this.price = price;
+            }
+
+            public Item(Item item)
+            {
+                this.name = item.name;
+                count = 1;
+                this.value = item.value;
+                this.options = item.options;
+                this.price = item.price;
+            }
+        }
+
+        class Store
+        {
+            public List<Item> itemList;
+            
+            public Store()
+            {
+                List<Item> itemList = new List<Item>();
+                itemList.Add(new Item("수련자의 갑옷", 5, "수련에 도움을 주는 갑옷입니다.", 1000));
+                itemList.Add(new Item("무쇠갑옷", 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 350));
+                itemList.Add(new Item("스파르타의 갑옷", 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500));
+                itemList.Add(new Item("낡은 검", 2, "쉽게 볼 수 있는 낡은 검 입니다.", 600));
+                itemList.Add(new Item("청동 도끼", 5, "어디선가 사용됐던거 같은 도끼입니다.", 1500));
+                itemList.Add(new Item("스파르타의 창", 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 350));
+            }
+        }
 
         static void Main(string[] args)
         {
             Console.Write("이름을 입력하세요 : ");
             name = Console.ReadLine();
-            mainScene();
 
+            while(true)
+            {
+                village();
+            }
         }
 
-        static void mainScene()
+        static void village()
         {
             Console.Clear();
             Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
@@ -30,23 +79,22 @@ namespace spartaDungeon
             Console.WriteLine("1. 상태 보기");
             Console.WriteLine("2. 인벤토리");
             Console.WriteLine("3. 상점");
-            Console.WriteLine("0. 게임종료");
 
             Console.WriteLine();
 
             Console.WriteLine("원하시는 행동을 입력해주세요.");
             Console.Write(">> ");
-            int num = int.Parse(Console.ReadLine());
+            int input = int.Parse(Console.ReadLine());
 
-            if (num == 1)
+            if (input == 1)
             {
                 charater();
             }
-            else if (num == 2)
+            else if (input == 2)
             {
                 inventory();
             }
-            else if (num == 3)
+            else if (input == 3)
             {
                 store();
             }
@@ -58,22 +106,26 @@ namespace spartaDungeon
 
         static void charater()
         {
+            Console.Clear();
             Console.WriteLine("상태 보기");
             Console.WriteLine("캐릭터의 정보가 표시됩니다.");
 
             Console.WriteLine("Lv. " + "{0:D2}",level);
-            Console.WriteLine("Chad : " + name);
+            Console.WriteLine("Chad : " + name + " ( 전사 )");
             Console.WriteLine("공격력 : " + attack);
             Console.WriteLine("방어력 : " + defense);
             Console.WriteLine("체 력 : " + hp);
             Console.WriteLine("Gold : " + gold + "G");
 
             Console.WriteLine("0. 나가기");
-            int charater_num = int.Parse(Console.ReadLine());
 
-            if (charater_num == 0)
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">> ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
             {
-                mainScene();
+                village();
             }
             else
             {
@@ -85,6 +137,7 @@ namespace spartaDungeon
 
         static void inventory()
         {
+            Console.Clear();
             Console.WriteLine("인벤토리");
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
 
@@ -93,13 +146,15 @@ namespace spartaDungeon
             Console.WriteLine("1. 장착 관리");
             Console.WriteLine("0. 나가기");
 
-            int inventory_num = int.Parse(Console.ReadLine());
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">> ");
+            int input = int.Parse(Console.ReadLine());
 
-            if (inventory_num == 0)
+            if (input == 0)
             {
-                mainScene();
+                village();
             }
-            else if (inventory_num == 1)
+            else if (input == 1)
             {
                 inventory_mounting();
             }
@@ -113,30 +168,19 @@ namespace spartaDungeon
 
         static void inventory_mounting()
         {
+            Console.Clear();
             Console.WriteLine("인벤토리 - 장착 관리");
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
-        }
 
-        static void store()
-        {
-            Console.WriteLine("상점");
-            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
-
-            Console.WriteLine("[보유 골드]");
-            Console.WriteLine("[아이템 목록]");
-
-            Console.WriteLine("1. 아이템 구매");
             Console.WriteLine("0. 나가기");
 
-            int store_num = int.Parse(Console.ReadLine());
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">> ");
+            int input = int.Parse(Console.ReadLine());
 
-            if (store_num == 0)
+            if (input == 0)
             {
-                mainScene();
-            }
-            else if (store_num == 1)
-            {
-                store_purchase();
+                inventory();
             }
             else
             {
@@ -145,9 +189,35 @@ namespace spartaDungeon
             }
         }
 
-        static void store_purchase()
+        static void store()
         {
-            Console.WriteLine("상점 - 아이템 구매");
+            Console.Clear();
+            Console.WriteLine("상점");
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+
+            Console.WriteLine("[보유 골드]");
+            Console.WriteLine("{0} G", gold);
+            Console.WriteLine("[아이템 목록]");
+
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("0. 나가기");
+
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">> ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
+            {
+                village();
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다.");
+                Console.ReadLine();
+            }
         }
+
+        
     }
 }
